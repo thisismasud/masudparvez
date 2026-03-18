6f9dc98ae76f77e9e16944828a58a2787bca17d6
thisismasud <thisismasud1@gmail.com>
2026-03-18 21:21:01 +0600

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,21 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+      ></script>
+      <script>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA_ID}');
+            `}
+      </script>
     </html>
   );
 }
